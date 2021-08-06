42feee13ec3bfceac7c8f25e1f6d01b7e6d08bc2
Isuru Fernando <isuruf@gmail.com>
2021-08-06 06:12:59 +0530

    use autotools_clang_conda on win

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
 
 ./configure --prefix=$PREFIX \
-            --with-gmp=$PREFIX \
-            --enable-static
+            --with-gmp=$PREFIX
+
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 make -j${CPU_COUNT}
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
