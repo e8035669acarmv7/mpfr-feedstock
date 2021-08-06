d353a5a4874e13445b019d68375b5d9b9853c308
Isuru Fernando <isuruf@gmail.com>
2021-08-06 07:21:24 +0530

    remove output-def

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* . || true
 
 if [[ "$target_platform" == "win-64" ]]; then
   export CPPFLAGS="$CPPFLAGS -DMSC_USE_DLL"
+  sed -i.bak "s@-Wl,--output-def,.libs/libmpfr-6.dll.def@@g" configure
 fi
 
 ./configure --prefix=$PREFIX \
